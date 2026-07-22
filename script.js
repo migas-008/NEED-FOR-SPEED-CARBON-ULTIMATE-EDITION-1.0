370d674ead189f37cc738485ff7622b642364137
Migas <igorcapucho615@gmail.com>
2026-07-22 10:57:04 +0100

    Add load event and button click alert

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// JavaScript do site
+// Need for Speed Carbon: Ultimate Edition
+
+window.addEventListener("load", () => {
+    console.log("Site carregado com sucesso!");
+});
+
+// Efeito ao clicar no botão de download
+const botao = document.querySelector("button");
+
+if (botao) {
+    botao.addEventListener("click", () => {
+        alert("O download da versão 1.0 estará disponível em breve.");
+    });
+}
